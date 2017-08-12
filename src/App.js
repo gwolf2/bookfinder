0ecd6afd0b9475f87ee6b3432e534d69dcde0a61
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-11 22:25:49 -0400

    Completed app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Grid, Col, Row} from 'react-bootstrap';
+import axios from 'axios';
+import Header from './Components/Header';
+import Books from './Components/Books';
+import SearchInput from './Components/SearchInput';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      books: [],
+      text: 'Harry Potter'
+    }
+  }
+
+  componentWillMount(){
+    this.getBooks();
+  }
+
+  getBooks(){
+    axios.request({
+      method: 'get',
+      url: 'https://www.googleapis.com/books/v1/volumes?q=' + this.state.text
+    }).then((response) => {
+      this.setState({books: response.data.items});
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  handleChange(text){
+    this.setState({text: text}, this.getBooks());
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Grid>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+              <SearchInput onChange={this.handleChange.bind(this)} value={this.state.text}/>
+              <Books books={this.state.books} />
+            </Col>
+
+          </Row>
+        </Grid>
       </div>
     );
   }
